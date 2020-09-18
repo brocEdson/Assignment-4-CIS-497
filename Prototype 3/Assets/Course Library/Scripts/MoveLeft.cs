8c6dc96be26f2d0a4ea11e6ae6f2b84c8f85609e
brocEdson <bedson@mail.bradley.edu>
2020-09-18 01:38:23 -0500

    Added MoveLeft and started SpawnManager

---
+++ Prototype 3/Assets/Course Library/Scripts/MoveLeft.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed = 30f;
+    private float leftBound = -15;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+        if(transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
